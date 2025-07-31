9a6a054348011241bc3efa4454c61df580adb720
Shawn <lattekun0@gmail.com>
2025-07-31 08:36:01 +0800

    wip: temporary save

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -3,11 +3,11 @@ import jwt, { SignOptions } from 'jsonwebtoken'
 const JWT_SECRET = process.env.JWT_SECRET
 if (!JWT_SECRET) throw new Error('JWT_SECRET 未設定')
 
-export const signToken = (payload: Record<string, any>, expiresIn: string | number = '1d') => {
-  const options: SignOptions = {
-    expiresIn: expiresIn as SignOptions['expiresIn'],
-  }
-  return jwt.sign(payload, JWT_SECRET, options)
+export const signToken = (
+  payload: Record<string, any>,
+  expiresIn: SignOptions['expiresIn'] = '1d'
+) => {
+  return jwt.sign(payload, JWT_SECRET, { expiresIn })
 }
 
 export const verifyToken = (token: string) => jwt.verify(token, JWT_SECRET)
